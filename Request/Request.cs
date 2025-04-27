db1eeaca8ac5ecb494addd69b0628e803364fd75
xavier xavi <xavier79997@gmail.com>
2025-04-27 12:36:49 +0530

    changes has been done

---
+++ Request/Request.cs
@@ -0,0 +1,15 @@
+﻿namespace Azure_API_Test.Request
+{
+    public class Request
+    {
+        public class LoginRequest
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        public class RefreshTokenRequest
+        {
+            public string RefreshToken { get; set; }
+        }
+    }
+}
---
+++ Tokens/Token_Generator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Azure_API_Test.Tokens
+{
+    public class Token_Generator
+    {
+        private readonly IConfiguration _config;
+        public Token_Generator(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GenerateToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+    }
+}
